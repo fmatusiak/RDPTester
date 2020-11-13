e9cd99975ddc82bd85d2a176a477f68c15277fe0
Filip <f.matusiak95@gmail.com>
2020-11-13 13:34:25 +0100

    add exists project

---
+++ RDPTester/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace RDPTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mtbUserPassword = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rComputersList = new System.Windows.Forms.RichTextBox();
+            this.bStartTest = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ZALOGOWANO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DATA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rLogi = new System.Windows.Forms.RichTextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.rdp = new AxMSTSCLib.AxMsRdpClient9NotSafeForScripting();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rdp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbUserName.Location = new System.Drawing.Point(28, 52);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(225, 22);
+            this.tbUserName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(88, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "UŻYTKOWNIK";
+            // 
+            // mtbUserPassword
+            // 
+            this.mtbUserPassword.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.mtbUserPassword.Location = new System.Drawing.Point(28, 107);
+            this.mtbUserPassword.Name = "mtbUserPassword";
+            this.mtbUserPassword.PasswordChar = '*';
+            this.mtbUserPassword.Size = new System.Drawing.Size(225, 22);
+            this.mtbUserPassword.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(106, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "HASŁO";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightGray;
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.rComputersList);
+            this.groupBox1.Controls.Add(this.mtbUserPassword);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbUserName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(893, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(276, 585);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "USTAWIENIA DO LOGOWANIA Z KOMPUTERAMI";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(147, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 14);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 14);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ILOŚĆ KOMPUTERÓW:";
+            // 
+            // rComputersList
+            // 
+            this.rComputersList.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rComputersList.Location = new System.Drawing.Point(28, 173);
+            this.rComputersList.Name = "rComputersList";
+            this.rComputersList.Size = new System.Drawing.Size(225, 406);
+            this.rComputersList.TabIndex = 5;
+            this.rComputersList.Text = "";
+            this.rComputersList.TextChanged += new System.EventHandler(this.rComputersList_TextChanged);
+            // 
+            // bStartTest
+            // 
+            this.bStartTest.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bStartTest.Location = new System.Drawing.Point(352, 603);
+            this.bStartTest.Name = "bStartTest";
+            this.bStartTest.Size = new System.Drawing.Size(237, 105);
+            this.bStartTest.TabIndex = 5;
+            this.bStartTest.Text = "ROZPOCZNIJ TEST";
+            this.bStartTest.UseVisualStyleBackColor = true;
+            this.bStartTest.Click += new System.EventHandler(this.bStartTest_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column1,
+            this.ZALOGOWANO,
+            this.DATA});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView1.Size = new System.Drawing.Size(875, 585);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column2.HeaderText = "NUMER";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column1
+            // 
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Column1.HeaderText = "NAZWA KOMPUTERA";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // ZALOGOWANO
+            // 
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ZALOGOWANO.DefaultCellStyle = dataGridViewCellStyle4;
+            this.ZALOGOWANO.HeaderText = "ZALOGOWANO";
+            this.ZALOGOWANO.Name = "ZALOGOWANO";
+            this.ZALOGOWANO.ReadOnly = true;
+            // 
+            // DATA
+            // 
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DATA.DefaultCellStyle = dataGridViewCellStyle5;
+            this.DATA.HeaderText = "DATA";
+            this.DATA.Name = "DATA";
+            this.DATA.ReadOnly = true;
+            // 
+            // rLogi
+            // 
+            this.rLogi.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rLogi.ForeColor = System.Drawing.Color.DarkRed;
+            this.rLogi.Location = new System.Drawing.Point(12, 603);
+            this.rLogi.Name = "rLogi";
+            this.rLogi.ReadOnly = true;
+            this.rLogi.Size = new System.Drawing.Size(255, 105);
+            this.rLogi.TabIndex = 8;
+            this.rLogi.Text = "";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "Działam w tle !";
+            this.notifyIcon1.BalloonTipTitle = "RDP TESTER";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "RDP Tester";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // rdp
+            // 
+            this.rdp.Enabled = true;
+            this.rdp.Location = new System.Drawing.Point(775, 603);
+            this.rdp.Name = "rdp";
+            this.rdp.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("rdp.OcxState")));
+            this.rdp.Size = new System.Drawing.Size(192, 192);
+            this.rdp.TabIndex = 7;
+            this.rdp.Visible = false;
+            this.rdp.OnLoginComplete += new System.EventHandler(this.rdp_OnLoginComplete);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(1181, 787);
+            this.Controls.Add(this.rLogi);
+            this.Controls.Add(this.rdp);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.bStartTest);
+            this.Controls.Add(this.groupBox1);
+            this.MaximumSize = new System.Drawing.Size(1197, 826);
+            this.Name = "Form1";
+            this.Text = "RDP TESTER";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rdp)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox mtbUserPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rComputersList;
+        private System.Windows.Forms.Button bStartTest;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ZALOGOWANO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DATA;
+        private AxMSTSCLib.AxMsRdpClient9NotSafeForScripting rdp;
+        private System.Windows.Forms.RichTextBox rLogi;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ RDPTester/Form1.cs
@@ -0,0 +1,273 @@
+﻿using MSTSCLib;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RDPTester
+{
+    public partial class Form1 : Form
+    {
+        private string directoryPath = @"C:\testerRDP";
+        private bool loginOK = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkDate()
+        {
+            DateTime now = DateTime.Now;
+            DateTime newDate = new DateTime(2020, 11, 21);
+
+            double diff = (newDate - now).TotalDays;
+
+            int days = (int)diff;
+
+            if (days <= 0)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void rComputersList_TextChanged(object sender, EventArgs e)
+        {
+            var lineCount = rComputersList.Lines.Count();
+            label4.Text = lineCount.ToString();
+        }
+
+        private void checkConnect(string server, string userName, string password)
+        {
+            try
+            {
+                rdp.Server = server;
+                rdp.UserName = userName;
+
+                IMsTscNonScriptable secured = (IMsTscNonScriptable)rdp.GetOcx();
+                secured.ClearTextPassword = password;
+                rdp.Connect();
+
+                Debug.WriteLine(rdp.Connected, "checkConnect");
+            }
+            catch (Exception) { };
+
+        }
+
+        private async Task<bool> isCheckLoginComplete()
+        {
+            /* await Task.Delay(6000);*/
+
+            Debug.WriteLine(rdp.Connected, "isCheckLoginComplete");
+            if (rdp.Connected.ToString() == "1")
+            {
+                rdp.Disconnect();
+                return true;
+            }
+            return false;
+        }
+
+        private bool checkUserAndPassword()
+        {
+            if (string.IsNullOrEmpty(tbUserName.Text.ToString()))
+            {
+                MessageBox.Show("NIE WYPEŁNIONO POLA UŻYTKOWNIK");
+                return false;
+            }
+            return true;
+        }
+
+        private async void bStartTest_Click(object sender, EventArgs e)
+        {
+            loginOK = false;
+
+            if (checkUserAndPassword())
+            {
+                DateTime dateFileName = DateTime.Now;
+                string correctDataFileName = dateFileName.ToString("yyyy_MM_dd_HH_mm_ss");
+
+                string fileName = "RaportRDP_" + correctDataFileName;
+
+                dataGridView1.Rows.Clear();
+                rLogi.Text = String.Empty;
+
+                tbUserName.Enabled = false;
+                mtbUserPassword.Enabled = false;
+                rComputersList.Enabled = false;
+                bStartTest.Enabled = false;
+                bStartTest.Text = "TRWA TEST ...";
+
+                int count = 1;
+                int proby = 1;
+
+                var lines = rComputersList.Lines;
+                var lineCount = rComputersList.Lines.Count();
+
+                for (int i = 0; i < lineCount; i++)
+                {
+                    rLogi.Text = String.Empty;
+                    DateTime now = DateTime.Now;
+                    string correctData = now.ToString("yyyy-MM-dd HH:mm:ss");
+
+                    string computerName = rComputersList.Lines[i];
+
+                    Debug.WriteLine(computerName, "computer name");
+
+                    rLogi.Text += "NAZWA KOMPUTERA: " + computerName + Environment.NewLine;
+
+                    while (proby < 4)
+                    {
+                        Debug.WriteLine(proby, "Próba");
+
+                        rLogi.Text += "PRÓBA ŁĄCZENIA: " + proby + Environment.NewLine;
+
+                        checkConnect(computerName, tbUserName.Text.ToString(), mtbUserPassword.Text.ToString());
+
+                        /* bool checkLoginComplete = await Task.Run(async () => await isCheckLoginComplete());*/
+                        await Task.Delay(6000);
+
+                        if (proby == 3 || loginOK)
+                        {
+                            if (loginOK)
+                            {
+                                saveUserAndPasswordToFile(fileName, computerName, "ZALOGOWANO", count.ToString());
+
+                                rLogi.Text += "ZALOGOWANO !!" + Environment.NewLine;
+                                addResultToDataGridView(count.ToString(), computerName, "ZALOGOWANO", correctData, false);
+                            }
+                            else
+                            {
+                                saveUserAndPasswordToFile(fileName, computerName, "BŁĄD LOGOWANIA", count.ToString());
+                                rLogi.Text += "BŁĄD LOGOWANIA !!" + Environment.NewLine;
+                                addResultToDataGridView(count.ToString(), computerName, "BŁĄD LOGOWANIA", correctData, true);
+                            }
+                            proby = 3;
+                        }
+                        proby++;
+                    }
+
+                    loginOK = false;
+                    proby = 1;
+                    count++;
+
+                    if (rdp.Connected.ToString() == "1")
+                    {
+                        rdp.Disconnect();
+                    }
+
+                }
+
+                tbUserName.Enabled = true;
+                mtbUserPassword.Enabled = true;
+                rComputersList.Enabled = true;
+                bStartTest.Enabled = true;
+                bStartTest.Text = "ROZPOCZNIJ TEST";
+            }
+        }
+
+        private void addResultToDataGridView(string computerNumber, string computerName, string result, string data, bool isError)
+        {
+            dataGridView1.Rows.Add(computerNumber, computerName, result, data);
+
+            int lastIndex = dataGridView1.Rows.Count - 2;
+
+            if (!isError)
+            {
+                DataGridViewRow row = dataGridView1.Rows[lastIndex];
+                row.DefaultCellStyle.BackColor = Color.Green;
+            }
+            else
+            {
+                DataGridViewRow row = dataGridView1.Rows[lastIndex];
+                row.DefaultCellStyle.BackColor = Color.Red;
+            }
+        }
+        private bool saveUserAndPasswordToFile(string fileName, string computerName, string computerStatus, string count)
+        {
+            string tmpFileName = directoryPath + @"\" + fileName + ".txt";
+
+            if (!Directory.Exists(directoryPath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(directoryPath);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("NIE UDAŁO SIĘ STWORZYĆ KATALOGU DO RAPORTÓW !! \n" + directoryPath, "BŁĄD", MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                    return false;
+                }
+            }
+
+            if (!File.Exists(tmpFileName))
+            {
+                try
+                {
+                    File.Create(tmpFileName).Dispose();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("NIE UDAŁO SIĘ STWORZYĆ PLIKU ", "BŁĄD", MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                    return false;
+                }
+            }
+
+            if (File.Exists(tmpFileName))
+            {
+                try
+                {
+                    File.AppendAllText(tmpFileName, count + ".      " + computerName + "            ");
+                    File.AppendAllText(tmpFileName, computerStatus);
+                    File.AppendAllText(tmpFileName, Environment.NewLine);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("NIE UDAŁO SIĘ ZAPISAĆ DANYCH DO PLIKU ", "BŁĄD", MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                    return false;
+                }
+
+            }
+            return true;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            checkDate();
+        }
+
+        private void rdp_OnLoginComplete(object sender, EventArgs e)
+        {
+            Debug.WriteLine("login ok");
+            loginOK = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(1000);
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+    }
+}
